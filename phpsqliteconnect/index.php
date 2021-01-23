8a32a37c5f7d904279fd18f8e1bed6db655b8227
youming lu <youminglu@youmings-air.devices.brown.edu>
2021-01-23 18:57:55 -0500

    removed connection check and redudant sqliteconnection call

--- phpsqliteconnect/index.php
+++ phpsqliteconnect/index.php
@@ -3,51 +3,47 @@ require 'vendor/autoload.php';
 
 use App\SQLiteConnection as SQLiteConnection;
 use App\SQLiteCreateTable as SQLiteCreateTable;
-$pdo = (new SQLiteConnection())->connect();
-if ($pdo != null)
-    echo 'Connected to the SQLite database successfully!';
-else
-    echo 'Whoops, could not connect to the SQLite database!';
 
-    $sqlite = new SQLiteCreateTable((new SQLiteConnection())->connect());
-    // create new tables
-    $sqlite->createTables();
-    // get the table list
-    $tables = $sqlite->getTableList();
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <meta name="description" content="">
-            <meta name="author" content="sqlitetutorial.net">
-            <title>PHP SQLite CREATE TABLE Demo</title>
-            <link href="http://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+$sqlite = new SQLiteCreateTable((new SQLiteConnection())->connect());
+// create new tables
+$sqlite->createTables();
+// get the table list
+$tables = $sqlite->getTableList();
+?>
 
-        </head>
-        <body>
-            <div class="container">
-                <div class="page-header">
-                    <h1>PHP SQLite CREATE TABLE Demo</h1>
-                </div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="sqlitetutorial.net">
+        <title>PHP SQLite CREATE TABLE Demo</title>
+        <link href="http://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Tables</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($tables as $table) : ?>
-                            <tr>
-
-                                <td><?php echo $table ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header">
+                <h1>PHP SQLite CREATE TABLE Demo</h1>
             </div>
-        </body>
-    </html>
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Tables</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($tables as $table) : ?>
+                        <tr>
+
+                            <td><?php echo $table ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
